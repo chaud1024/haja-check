bbccfec8f8721451fa34ab66dd8c077f8d167468
chaud1024 <chaud1024@gmail.com>
2024-04-17 17:52:26 +0900

    set BrowserRouter, Routes, Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import Layout from "@components/Layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import NavBar from "@components/shared/NavBar";
+import Home from "@pages/Home";
+import Layout from "@pages/Layout";
+import MyPage from "@pages/MyPage";
+import ModalLogin from "./components/Modal/ModalLogin";
 
 function App() {
-  return <Layout />;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/list" element={<Layout />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+      <ModalLogin />
+    </BrowserRouter>
+  );
 }
 
 export default App;
