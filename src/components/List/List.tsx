3b7c8ea81562ab2ea0df61ad3831008e92bfddbe
chaud1024 <chaud1024@gmail.com>
2024-04-18 17:53:11 +0900

    edit List

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -4,13 +4,13 @@ const List = ({ haja }: { haja: HajaItem[] }) => {
   return (
     <div>
       <h2>하자 목록</h2>
-      {haja?.map((h, index) => (
-        <div key={index}>
+      {haja?.map((h, i) => (
+        <div key={i}>
           <span>
-            {index + 1}
+            {i + 1}
             {")"}
           </span>
-          <span>{h?.cate} : </span>
+          <span>{h?.index} : </span>
           <span>{h?.description}</span>
         </div>
       ))}
